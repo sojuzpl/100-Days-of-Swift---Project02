0f234487005a264dcb7244455f2eb002358cee63
Zbyszek <zpietras@icloud.com>
2019-02-22 16:58:55 +0100

    step 03 finished - Making the basic game work - UIButton and CALayer

--- Project02/ViewController.swift
+++ Project02/ViewController.swift
@@ -11,14 +11,36 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var button1: UIButton!
-    
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // buttons setup
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+
+        askQuestion()
+        
+        
+    }
+    
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button1.setImage(UIImage(named: countries[1]), for: .normal)
+        button1.setImage(UIImage(named: countries[2]), for: .normal)
+
     }
 
 
