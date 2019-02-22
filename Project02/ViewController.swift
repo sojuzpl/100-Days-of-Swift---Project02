300862dd5d8616678ffa32616090fce4c7fbdd2f
Zbyszek <zpietras@icloud.com>
2019-02-22 16:24:47 +0100

    Initial Commit

---
+++ Project02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project02
+//
+//  Created by Zbigniew Pietras on 22/02/2019.
+//  Copyright © 2019 Zbigniew Pietras. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
