190bc72a72eaa9474df428e28d48eb1c42618c11
Zbyszek <zpietras@icloud.com>
2019-02-22 22:48:22 +0100

    step 4 finished - Guess which flag: random numbers + code fixes

--- Project02/ViewController.swift
+++ Project02/ViewController.swift
@@ -14,11 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
-    var countries = ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         // Do any additional setup after loading the view, typically from a nib.
         
         // buttons setup
@@ -37,9 +40,16 @@ class ViewController: UIViewController {
     
     
     func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
-        button1.setImage(UIImage(named: countries[1]), for: .normal)
-        button1.setImage(UIImage(named: countries[2]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
 
     }
 
